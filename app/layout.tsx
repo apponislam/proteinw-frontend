8a38f8692fddcded6acac78c04226086ca65004c
Appon Islam <appon2003@gmail.com>
2026-06-10 06:52:51 +0600

    layout updated for toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,27 @@ export default function RootLayout({
                     <TooltipProvider>
                         {children}
 
-                        <Toaster />
+                        <Toaster
+                            position="top-right"
+                            richColors
+                            closeButton={true}
+                            expand={false}
+                            visibleToasts={5}
+                            toastOptions={{
+                                classNames: {
+                                    toast: "bg-white border border-[#FFDEA8] text-[#1A1C1C] shadow-lg rounded-xl",
+                                    title: "font-semibold text-[#1A1C1C]",
+                                    description: "text-gray-600",
+                                    success: "border-green-600 bg-green-50 text-green-900",
+                                    error: "border-red-500 bg-red-50 text-red-900",
+                                    warning: "border-yellow-500 bg-yellow-50 text-yellow-900",
+                                    info: "border-blue-500 bg-blue-50 text-blue-900",
+                                    actionButton: "bg-[#7C5800] text-white hover:bg-[#654700]",
+                                    cancelButton: "bg-gray-100 text-gray-700 hover:bg-gray-200",
+                                    closeButton: "bg-transparent text-gray-500 hover:text-gray-700",
+                                },
+                            }}
+                        />
                     </TooltipProvider>
                 </ReduxProviders>
             </body>
