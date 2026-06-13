29470f7bae3085faabb6d115a9794eaf4afc5d66
Appon Islam <appon2003@gmail.com>
2026-06-13 22:05:22 +0600

    new ip added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ["10.10.7.24", "fundraising.apponislam.top", "198.41.192.7"],
+    allowedDevOrigins: ["10.10.7.24", "fundraising.apponislam.top", "198.41.192.7", "10.10.26.171"],
 };
 
 export default nextConfig;
